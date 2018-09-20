e7685a6b65f27ad464de16e542574d61dd25d68b
Paepke.Celina <Paepke.Celina@SK-448A5BCB8A8B.2631.skool.net>
2018-09-20 09:15:28 +0200

    Fizzbuzz!

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,48 @@
+public class HelloWorld 
+{
 
-public class HelloWorld {
+	
+	public static void main(String args[])
+	{
+	    int zaehler=100; //Anzahl der Zahlenfolgen
+        int zahl=1;  //Zahl mit der das Programm startet
+  while(zaehler>=1)  //solange der zaehler nicht abgelaufen ist...
+  {
+	   if(zahl%3==0) //...ueberpruefe ob Zahl durch 3 teilbar..
+	   {
+		 
+		  if(zahl%5==0)  //...falls auch durch 5 teilbar
+		  {
+			  System.out.print("fisbas");   //... schreibe fisbas
+		  }else
+		  {
+			  System.out.print("fis");   //wenn nicht durch 5 teilbar schreibe "fis"
+		  }
+	   }else
+	   {
+	      if(zahl%5==0) //falls durch 5 teilbar
+	      {
+          System.out.print("bas"); //schreibe "bas"
+	      }else                      //wenn weder durch 5 noch durch 3 teilbar...
+	      {
+	       System.out.print(zahl);  // schreibe "Zahl"
+	      
+	      }
+	   }
+	 System.out.print(",");   // Kommas und Zeilenumbruch zur veranschaulichung
+	 System.out.println();
+	 zahl=zahl+1;            //nächste Zahl abfragem
+    zaehler=zaehler-1;       //zaehler eins runter setzen
+   
+	
+  }
+ 
+   }
+}
 
-	public static void main(String[] args) {
-	int zahl = 7;
-	while(zahl>1){
-	if(zahl %2!= 0 ){
-	zahl=zahl*3+1;	
-	}else {
-		zahl=zahl/2;
-	}
-	System.out.print(zahl+",");
-	}
-}
-}
+/* 
+ * methoden: public static boolean dividingby3
+ *           {
+ *           if(zahl%3==0)
+ *           {
+ */
